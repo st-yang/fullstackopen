bb28b665e1b48dc059f63ec8f92c47ab4968c4bf
YANG Shengtao <eranikus424@gmail.com>
2024-07-31 15:54:02 +0800

    2.5: Separate module step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,39 +1,4 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
-
-const Part = ({ part, exercise }) => {
-  return (
-    <p>
-      {part} {exercise}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part.name} exercise={part.exercises} />
-      ))}
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <b>total of {total} exercises</b>
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,13 @@
+import Part from './Part'
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercise={part.exercises} />
+      ))}
+    </div>
+  )
+}
+
+export default Content
