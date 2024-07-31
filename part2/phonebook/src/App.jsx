e88eee399391e290eef963f406505f9bf7f556e9
YANG Shengtao <eranikus424@gmail.com>
2024-07-31 23:18:17 +0800

    2.15*: The Phonebook step 10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -24,8 +24,17 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+
+    const person = persons.find((person) => person.name === newName)
+    if (person) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const updatedPerson = { ...person, number: newNumber }
+        personService.update(person.id, updatedPerson).then((returnedPerson) => {
+          setPersons(persons.map((p) => (p.id === person.id ? returnedPerson : p)))
+          setNewName('')
+          setNewNumber('')
+        })
+      }
     } else {
       personService.create({ name: newName, number: newNumber }).then((returnedPerson) => {
         setPersons(persons.concat(returnedPerson))
