9f0bedc0667709877b51d1c53bb41d3788626e99
YANG Shengtao <eranikus424@gmail.com>
2024-07-31 17:46:55 +0800

    2.7: The Phonebook Step 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,8 +10,12 @@ const App = () => {
 
   const handleNewNameSubmit = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({ name: newName }))
-    setNewName('')
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat({ name: newName }))
+      setNewName('')
+    }
   }
 
   return (
