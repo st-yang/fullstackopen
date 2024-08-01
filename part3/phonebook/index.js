5121c268e9ce39fa2d5658012edb27f13576a303
YANG Shengtao <eranikus424@gmail.com>
2024-08-01 22:05:36 +0800

    3.6: Phonebook backend step 6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -58,16 +58,22 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
+  if (!body.name || !body.number) {
     return response.status(400).json({
-      error: 'name missing'
+      error: 'name or number missing'
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
     })
   }
 
   const person = {
     id: generateId(),
     name: body.name,
-    number: body.number || '',
+    number: body.number,
   }
 
   persons = persons.concat(person)
