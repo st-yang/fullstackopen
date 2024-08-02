abb29557f195200bd49aab3fdc739a51fb195ac1
YANG Shengtao <eranikus424@gmail.com>
2024-08-02 14:21:07 +0800

    connect notes backend to MongoDB

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -1,5 +1,31 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
+// mongo 
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI;
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
+// mongo 
 
 let notes = [
   {
@@ -45,7 +71,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 const generateId = () => {
