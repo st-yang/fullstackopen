243f9396653d16bd5cc595bb74d753df88658768
YANG Shengtao <eranikus424@gmail.com>
2024-08-02 17:20:47 +0800

    3.18*: Phonebook database step 6

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -24,36 +24,15 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/info', (request, response) => {
-  response.send(`
-    <div>
-      <p>Phonebook has info for ${persons.length} people</p>
-      <p>${new Date().toString()}</p>
-    </div>
-  `)
+  Person.find({}).then(persons => {
+    response.send(`
+      <div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date().toString()}</p>
+      </div>
+    `)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
