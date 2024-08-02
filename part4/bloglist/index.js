bcb6cb1a35c64b1d417e33bd56cc4f3956be447e
YANG Shengtao <eranikus424@gmail.com>
2024-08-02 23:20:47 +0800

    4.1 Blog List, step 1 - make bloglist runnable

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -12,7 +13,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
