dadb8a60107122d36d335deee099cb5a01df9ff3
YANG Shengtao <eranikus424@gmail.com>
2024-08-02 14:50:25 +0800

    move db config to dedicated module for notes backend

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -3,29 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-// mongo 
-const mongoose = require('mongoose')
-
-const url = process.env.MONGODB_URI;
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
-// mongo 
+const Note = require('./models/note')
 
 let notes = [
   {
