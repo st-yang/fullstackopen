5e13e801846770d312b80881222021a08b124ad6
YANG Shengtao <eranikus424@gmail.com>
2024-08-05 21:09:33 +0800

    part4c link user when posting note data, and populate user/note when getting the other

--- notes/backend/controllers/users.js
+++ notes/backend/controllers/users.js
@@ -4,6 +4,7 @@ const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({})
+    .populate('notes', { content: 1, important: 1 })
   response.json(users)
 })
 
