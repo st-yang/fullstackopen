c0fc35c8e426e57c7f3afde15d6fdbe9e33ee02d
YANG Shengtao <eranikus424@gmail.com>
2024-08-05 22:19:44 +0800

    part4d add login endpoint with tests

---
+++ notes/backend/tests/login_api.test.js
@@ -0,0 +1,56 @@
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const bcrypt = require('bcrypt')
+const app = require('../app')
+const api = supertest(app)
+
+const helper = require('./test_helper')
+
+const User = require('../models/user')
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('login as root', async () => {
+    const user = {
+      username: 'root',
+      password: 'sekret',
+    }
+
+    await api
+      .post('/api/login')
+      .send(user)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(user.username))
+  })
+
+  test('login with invalid credentials', async () => {
+    const user = {
+      username: 'invalid',
+      password: 'invalid',
+    }
+
+    await api
+      .post('/api/login')
+      .send(user)
+      .expect(401)
+      .expect('Content-Type', /application\/json/)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
