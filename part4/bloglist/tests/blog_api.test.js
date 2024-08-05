172227a17689c33f473b930af908331e2123e4c6
YANG Shengtao <eranikus424@gmail.com>
2024-08-05 16:18:00 +0800

    4.8: Blog List Tests, step 1

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,42 @@
+const { test, describe, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('blog list api tests', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+    assert(titles.includes('Go To Statement Considered Harmful'))
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
