a7a2719a41e3fe94b31d8af0cad150d028f021b0
YANG Shengtao <eranikus424@gmail.com>
2024-08-07 01:04:01 +0800

    part5b define PropTypes for notes frontend

--- notes/frontend/src/components/Togglable.jsx
+++ notes/frontend/src/components/Togglable.jsx
@@ -1,4 +1,5 @@
 import { useState, forwardRef, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
 const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false)
@@ -29,6 +30,9 @@ const Togglable = forwardRef((props, refs) => {
   )
 })
 
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+}
 Togglable.displayName = 'Togglable'
 
 export default Togglable
