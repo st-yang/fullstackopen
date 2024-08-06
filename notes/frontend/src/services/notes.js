ed4866ddc48b27240bdb31793330f9cc4db5e714
YANG Shengtao <eranikus424@gmail.com>
2024-08-07 05:22:53 +0800

    part5d Controlling the state of the database

--- notes/frontend/src/services/notes.js
+++ notes/frontend/src/services/notes.js
@@ -16,13 +16,15 @@ const create = async newObject => {
   const config = {
     headers: { Authorization: token },
   }
-
   const response = await axios.post(baseUrl, newObject, config)
   return response.data
 }
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  const config = {
+    headers: { Authorization: token },
+  }
+  const request = axios.put(`${baseUrl}/${id}`, newObject, config)
   return request.then(response => response.data)
 }
 
