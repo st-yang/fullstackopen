dcca3ea1f304212e254c04ad438cea8d47dfebfc
YANG Shengtao <eranikus424@gmail.com>
2024-08-07 05:28:49 +0800

    part5d Test for failed login

--- notes/frontend/src/App.jsx
+++ notes/frontend/src/App.jsx
@@ -64,7 +64,7 @@ const App = () => {
       noteService.setToken(user.token)
       setUser(user)
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
+      setErrorMessage('wrong credentials')
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
