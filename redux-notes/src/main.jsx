3b917d00119fced15bc6dff05035b0f0084ae587
YANG Shengtao <eranikus424@gmail.com>
2024-08-07 23:22:42 +0800

    part6a Action creators

--- redux-notes/src/main.jsx
+++ redux-notes/src/main.jsx
@@ -9,25 +9,33 @@ const store = createStore(noteReducer)
 const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 
 const App = () => {
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    store.dispatch({
+  const createNote = (content) => {
+    return {
       type: 'NEW_NOTE',
       payload: {
         content,
         important: false,
         id: generateId(),
       },
-    })
+    }
+  }
+
+  const toggleImportanceOf = (id) => {
+    return {
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id },
+    }
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch(createNote(content))
   }
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: 'TOGGLE_IMPORTANCE',
-      payload: { id },
-    })
+    store.dispatch(toggleImportanceOf(id))
   }
 
   return (
