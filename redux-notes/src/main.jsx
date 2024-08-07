fadc8792383398ef0d970e1c70633f9973c69622
YANG Shengtao <eranikus424@gmail.com>
2024-08-07 23:19:10 +0800

    part6a Uncontrolled form

--- redux-notes/src/main.jsx
+++ redux-notes/src/main.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createStore } from 'redux'
+import noteReducer from './reducers/noteReducer'
+
+const store = createStore(noteReducer)
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
+const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    })
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id },
+    })
+  }
+
+  return (
+    <div>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
