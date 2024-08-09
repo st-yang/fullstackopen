41c848ad826d146762b2b404218fe0c8036c4df1
YANG Shengtao <eranikus424@gmail.com>
2024-08-09 23:58:24 +0800

    part6d Defining the counter context in a separate file

--- hook-counter/src/components/Button.jsx
+++ hook-counter/src/components/Button.jsx
@@ -1,8 +1,7 @@
-import { useContext } from 'react'
-import CounterContext from '../CounterContext'
+import { useCounterDispatch } from '../CounterContext'
 
 const Button = ({ type, label }) => {
-  const [counter, dispatch] = useContext(CounterContext)
+  const dispatch = useCounterDispatch()
   return <button onClick={() => dispatch({ type })}>{label}</button>
 }
 
