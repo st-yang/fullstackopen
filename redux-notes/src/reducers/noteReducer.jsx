b2b779229735f6d96abb747467f65ec46ae41f5c
YANG Shengtao <eranikus424@gmail.com>
2024-08-09 12:05:13 +0800

    part6b finishing the filters

--- redux-notes/src/reducers/noteReducer.jsx
+++ redux-notes/src/reducers/noteReducer.jsx
@@ -1,8 +1,21 @@
-const noteReducer = (state = [], action) => {
+const initialState = [
+  {
+    content: 'reducer defines how redux store works',
+    important: true,
+    id: 1,
+  },
+  {
+    content: 'state of store can contain any data',
+    important: false,
+    id: 2,
+  },
+]
+
+const noteReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'NEW_NOTE':
       return [...state, action.payload]
-    case 'TOGGLE_IMPORTANCE': {
+    case 'TOGGLE_IMPORTANCE':
       const id = action.payload.id
       const noteToChange = state.find((n) => n.id === id)
       const changedNote = {
@@ -10,7 +23,6 @@ const noteReducer = (state = [], action) => {
         important: !noteToChange.important,
       }
       return state.map((note) => (note.id !== id ? note : changedNote))
-    }
     default:
       return state
   }
