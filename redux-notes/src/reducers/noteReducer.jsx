1afcba58369c34f5ace3aec8a77abade97d5911c
YANG Shengtao <eranikus424@gmail.com>
2024-08-09 18:21:00 +0800

    part6c Asynchronous actions and Redux Thunk

--- redux-notes/src/reducers/noteReducer.jsx
+++ redux-notes/src/reducers/noteReducer.jsx
@@ -1,14 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+import noteService from '../services/notes'
 
 const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
   reducers: {
-    createNote(state, action) {
-      state.push(action.payload)
-    },
     toggleImportanceOf(state, action) {
       const id = action.payload
       const noteToChange = state.find((n) => n.id === id)
@@ -18,11 +14,29 @@ const noteSlice = createSlice({
       }
       return state.map((note) => (note.id !== id ? note : changedNote))
     },
+    appendNote(state, action) {
+      state.push(action.payload)
+    },
     setNotes(state, action) {
       return action.payload
     },
   },
 })
 
-export const { createNote, toggleImportanceOf, setNotes } = noteSlice.actions
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = (content) => {
+  return async (dispatch) => {
+    const newNote = await noteService.createNew(content)
+    dispatch(appendNote(newNote))
+  }
+}
+
 export default noteSlice.reducer
