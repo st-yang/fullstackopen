d3418d40190fd3c86a87909160be6193f1ab4b35
YANG Shengtao <eranikus424@gmail.com>
2024-08-09 15:34:19 +0800

    Revert "part6b convert filterReducer to use redux toolkit too"
    This reverts commit ae5cae9a44654c7b44dde6bc96e5d5c0efbabe29.

--- redux-notes/src/reducers/filterReducer.jsx
+++ redux-notes/src/reducers/filterReducer.jsx
@@ -1,14 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit'
-
-const filterSlice = createSlice({
-  name: 'filter',
-  initialState: 'ALL',
-  reducers: {
-    filterChange(state, action) {
+const filterReducer = (state = 'ALL', action) => {
+  switch (action.type) {
+    case 'SET_FILTER':
       return action.payload
-    },
-  },
-})
+    default:
+      return state
+  }
+}
 
-export const { filterChange } = filterSlice.actions
-export default filterSlice.reducer
+export const filterChange = (filter) => {
+  return {
+    type: 'SET_FILTER',
+    payload: filter,
+  }
+}
+
+export default filterReducer
