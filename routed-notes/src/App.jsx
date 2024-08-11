a32cb1ce784cf17198ff2d60dd12d567cc8861f4
YANG Shengtao <eranikus424@gmail.com>
2024-08-11 14:11:54 +0800

    part7a Parameterized route revisited

--- routed-notes/src/App.jsx
+++ routed-notes/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import { BrowserRouter as Router, Routes, Route, Link, Navigate, useParams, useNavigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, useNavigate, useMatch } from 'react-router-dom'
 
 const Home = () => (
   <div>
@@ -16,9 +16,7 @@ const Home = () => (
   </div>
 )
 
-const Note = ({ notes }) => {
-  const id = useParams().id
-  const note = notes.find((n) => n.id === Number(id))
+const Note = ({ note }) => {
   return (
     <div>
       <h2>{note.content}</h2>
@@ -111,36 +109,37 @@ const App = () => {
     padding: 5,
   }
 
+  const match = useMatch('/notes/:id')
+  const note = match ? notes.find((note) => note.id === Number(match.params.id)) : null
+
   return (
     <div>
-      <Router>
-        <div>
-          <Link style={padding} to='/'>
-            home
+      <div>
+        <Link style={padding} to='/'>
+          home
+        </Link>
+        <Link style={padding} to='/notes'>
+          notes
+        </Link>
+        <Link style={padding} to='/users'>
+          users
+        </Link>
+        {user ? (
+          <em>{user} logged in</em>
+        ) : (
+          <Link style={padding} to='/login'>
+            login
           </Link>
-          <Link style={padding} to='/notes'>
-            notes
-          </Link>
-          <Link style={padding} to='/users'>
-            users
-          </Link>
-          {user ? (
-            <em>{user} logged in</em>
-          ) : (
-            <Link style={padding} to='/login'>
-              login
-            </Link>
-          )}
-        </div>
+        )}
+      </div>
 
-        <Routes>
-          <Route path='/notes/:id' element={<Note notes={notes} />} />
-          <Route path='/notes' element={<Notes notes={notes} />} />
-          <Route path='/users' element={user ? <Users /> : <Navigate replace to='/login' />} />
-          <Route path='/login' element={<Login onLogin={login} />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path='/notes/:id' element={<Note note={note} />} />
+        <Route path='/notes' element={<Notes notes={notes} />} />
+        <Route path='/users' element={user ? <Users /> : <Navigate replace to='/login' />} />
+        <Route path='/login' element={<Login onLogin={login} />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
       <div>
         <br />
         <em>Note app, Department of Computer Science 2023</em>
--- routed-notes/src/main.jsx
+++ routed-notes/src/main.jsx
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Router } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>,
+)
