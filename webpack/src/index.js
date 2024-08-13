82144c65fbc9ad3f51c19871aa0703b28012755f
YANG Shengtao <eranikus424@gmail.com>
2024-08-13 23:29:27 +0800

    part7d Polyfill

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,5 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import PromisePolyfill from 'promise-polyfill'
+
+if (!window.Promise) {
+  window.Promise = PromisePolyfill
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
