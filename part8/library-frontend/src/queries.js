61f6715f003eb25e117e9ef012de2f18c3064e3e
YANG Shengtao <eranikus424@gmail.com>
2024-08-15 23:52:46 +0800

    8.9: Books view

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -10,3 +10,15 @@ export const ALL_AUTHORS = gql`
     }
   }
 `
+
+export const ALL_BOOKS = gql`
+  query allBooks($author: String, $genre: String) {
+    allBooks(author: $author, genre: $genre) {
+      title
+      author
+      published
+      genres
+      id
+    }
+  }
+`
