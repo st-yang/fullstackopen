c7fe7153a75502cc1318e28253fdf9387d5bfed2
YANG Shengtao <eranikus424@gmail.com>
2024-08-16 21:21:13 +0800

    8.19 Books by genre, part 1

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import { useQuery } from '@apollo/client'
 
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
+  const [genre, setGenre] = useState('')
   const result = useQuery(ALL_BOOKS)
 
   if (result.loading) {
@@ -14,10 +16,17 @@ const Books = (props) => {
   }
 
   const books = result.data.allBooks
+  const genres = [...new Set(books.flatMap((book) => book.genres))]
 
   return (
     <div>
       <h2>books</h2>
+
+      {genre && (
+        <p>
+          in genre <b>{genre}</b>
+        </p>
+      )}
 
       <table>
         <tbody>
@@ -26,15 +35,24 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((b) => (genre ? b.genres.includes(genre) : true))
+            .map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+
+      {genres.map((genre) => (
+        <button key={genre} onClick={() => setGenre(genre)}>
+          {genre}
+        </button>
+      ))}
+      <button onClick={() => setGenre('')}>all genres</button>
     </div>
   )
 }
