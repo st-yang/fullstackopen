2212f9271135f4451b320c7d295946d00126072b
YANG Shengtao <eranikus424@gmail.com>
2024-08-17 00:48:09 +0800

    8.23: Subscriptions - separate resolvers and schema from library-backend server

---
+++ part8/library-backend/resolvers.js
@@ -0,0 +1,150 @@
+const { GraphQLError } = require('graphql')
+const jwt = require('jsonwebtoken')
+
+const Author = require('./models/author')
+const Book = require('./models/book')
+const User = require('./models/user')
+
+const resolvers = {
+  Query: {
+    bookCount: () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
+    allAuthors: async () => Author.find({}),
+    allBooks: async (root, args) => {
+      const filter = {}
+
+      if (args.author) {
+        filter.author = await Author.findOne({ name: args.author })
+      }
+      if (args.genre) {
+        filter.genres = args.genre
+      }
+
+      const books = await Book.find(filter).populate('author')
+      return books
+    },
+    allGenres: async () => {
+      const books = await Book.find({})
+      const genres = books.reduce((acc, book) => {
+        book.genres.forEach((genre) => {
+          if (!acc.includes(genre)) {
+            acc.push(genre)
+          }
+        })
+        return acc
+      }, [])
+      return genres
+    },
+    me: (root, args, context) => context.currentUser,
+  },
+  Author: {
+    bookCount: async (author) => {
+      const books = await Book.find({ author: author.id })
+      return books.length
+    },
+  },
+  Mutation: {
+    addBook: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        })
+      }
+
+      let author = await Author.findOne({ name: args.author })
+      if (!author) {
+        author = new Author({ name: args.author })
+        try {
+          await author.save()
+        } catch (error) {
+          throw new GraphQLError('Saving author failed', {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.name,
+              error,
+            },
+          })
+        }
+      }
+
+      const book = new Book({ ...args, author: author.id })
+      try {
+        await book.save()
+      } catch (error) {
+        throw new GraphQLError('Saving book failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error,
+          },
+        })
+      }
+
+      const populatedBook = await book.populate('author')
+      return populatedBook
+    },
+    editAuthor: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        })
+      }
+
+      const author = await Author.findOne({ name: args.name })
+      if (!author) {
+        return null
+      }
+
+      author.born = args.setBornTo
+      try {
+        await author.save()
+      } catch (error) {
+        throw new GraphQLError('Saving author failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error,
+          },
+        })
+      }
+      return author
+    },
+    createUser: async (root, args) => {
+      const user = new User({ ...args })
+
+      return user.save().catch((error) => {
+        throw new GraphQLError('Creating user failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.username,
+            error,
+          },
+        })
+      })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'secret') {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        })
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+    },
+  },
+}
+
+module.exports = resolvers
