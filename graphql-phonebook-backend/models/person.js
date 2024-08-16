216aab6c1915ab850d320e7e195052f21aff905f
YANG Shengtao <eranikus424@gmail.com>
2024-08-16 19:47:28 +0800

    part8d fix uniqueness in person/user

--- graphql-phonebook-backend/models/person.js
+++ graphql-phonebook-backend/models/person.js
@@ -1,25 +1,30 @@
 const mongoose = require('mongoose')
 
+const uniqueValidator = require('mongoose-unique-validator')
+
 const schema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 5
+    unique: true,
+    minlength: 5,
   },
   phone: {
     type: String,
-    minlength: 5
+    minlength: 5,
   },
   street: {
     type: String,
     required: true,
-    minlength: 5
+    minlength: 5,
   },
   city: {
     type: String,
     required: true,
-    minlength: 3
+    minlength: 3,
   },
 })
+
+schema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Person', schema)
--- graphql-phonebook-backend/models/user.js
+++ graphql-phonebook-backend/models/user.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose')
 
+const uniqueValidator = require('mongoose-unique-validator')
+
 const schema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    minlength: 3
+    unique: true,
+    minlength: 3,
   },
   friends: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Person'
-    }
+      ref: 'Person',
+    },
   ],
 })
+
+schema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('User', schema)
