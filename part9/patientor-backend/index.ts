8f694393ac23359a0954dc4d7e900a637256a008
YANG Shengtao <eranikus424@gmail.com>
2024-08-19 20:36:39 +0800

    9.9: Patientor backend, step2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,5 +1,10 @@
 import express from 'express'
+import cors from 'cors'
+
 const app = express()
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors())
 app.use(express.json())
 
 const PORT = 3001
@@ -7,6 +12,14 @@ const PORT = 3001
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here')
   res.send('pong')
+})
+
+app.get('/api/patients', (_req, res) => {
+  res.status(200).send()
+})
+
+app.post('/api/patients', (_req, res) => {
+  res.status(201).send()
 })
 
 app.listen(PORT, () => {
