5221136c0216a9a43431cd6f0e1154fd2c8dcb98
YANG Shengtao <eranikus424@gmail.com>
2024-08-19 21:39:28 +0800

    9.10: Patientor backend, step3

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 
+import diagnoseRouter from './routes/diagnoses'
+
 const app = express()
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors())
 app.use(express.json())
 
@@ -21,6 +22,8 @@ app.get('/api/patients', (_req, res) => {
 app.post('/api/patients', (_req, res) => {
   res.status(201).send()
 })
+
+app.use('/api/diagnoses', diagnoseRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
---
+++ part9/patientor-backend/src/services/diagnoseService.ts
@@ -0,0 +1,10 @@
+import diagnoses from '../../data/diagnoses'
+import { DiagnoseEntry } from '../types'
+
+const getEntries = (): DiagnoseEntry[] => {
+  return diagnoses
+}
+
+export default {
+  getEntries,
+}
