995b306731ef8e455aec1010e7389b5845371821
YANG Shengtao <eranikus424@gmail.com>
2024-08-20 22:02:03 +0800

    part9d React app with state

--- typed-notes/src/App.tsx
+++ typed-notes/src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { Note } from './types'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [newNote, setNewNote] = useState('')
+  const [notes, setNotes] = useState<Note[]>([{ id: '1', content: 'testing' }])
+
+  const noteCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    const noteToAdd = {
+      content: newNote,
+      id: String(notes.length + 1),
+    }
+    setNotes(notes.concat(noteToAdd))
+    setNewNote('')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <form onSubmit={noteCreation}>
+        <input value={newNote} onChange={(event) => setNewNote(event.target.value)} />
+        <button type='submit'>add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
