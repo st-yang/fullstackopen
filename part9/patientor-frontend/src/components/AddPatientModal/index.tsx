e63c0517cd71739ccf1e95426f25ad0ca48de8ab
YANG Shengtao <eranikus424@gmail.com>
2024-08-21 14:20:31 +0800

    part9e format patientor-frontend with lint and prettier

--- part9/patientor-frontend/src/components/AddPatientModal/index.tsx
+++ part9/patientor-frontend/src/components/AddPatientModal/index.tsx
@@ -1,13 +1,13 @@
-import { Dialog, DialogTitle, DialogContent, Divider, Alert } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, Divider, Alert } from '@mui/material'
 
-import AddPatientForm from "./AddPatientForm";
-import { PatientFormValues } from "../../types";
+import AddPatientForm from './AddPatientForm'
+import { PatientFormValues } from '../../types'
 
 interface Props {
-  modalOpen: boolean;
-  onClose: () => void;
-  onSubmit: (values: PatientFormValues) => void;
-  error?: string;
+  modalOpen: boolean
+  onClose: () => void
+  onSubmit: (values: PatientFormValues) => void
+  error?: string
 }
 
 const AddPatientModal = ({ modalOpen, onClose, onSubmit, error }: Props) => (
@@ -15,10 +15,10 @@ const AddPatientModal = ({ modalOpen, onClose, onSubmit, error }: Props) => (
     <DialogTitle>Add a new patient</DialogTitle>
     <Divider />
     <DialogContent>
-      {error && <Alert severity="error">{error}</Alert>}
-      <AddPatientForm onSubmit={onSubmit} onCancel={onClose}/>
+      {error && <Alert severity='error'>{error}</Alert>}
+      <AddPatientForm onSubmit={onSubmit} onCancel={onClose} />
     </DialogContent>
   </Dialog>
-);
+)
 
-export default AddPatientModal;
+export default AddPatientModal
