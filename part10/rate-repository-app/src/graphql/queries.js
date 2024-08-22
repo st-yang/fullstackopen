23c8c3a3845958c4b8f1837d4957d24172c1c5a0
YANG Shengtao <eranikus424@gmail.com>
2024-08-22 18:45:06 +0800

    Exercise 10.11: fetching repositories with Apollo Client

--- part10/rate-repository-app/src/graphql/queries.js
+++ part10/rate-repository-app/src/graphql/queries.js
@@ -3,7 +3,20 @@ import { gql } from '@apollo/client'
 export const GET_REPOSITORIES = gql`
   query {
     repositories {
-    
+      totalCount
+      edges {
+        node {
+          id
+          fullName
+          description
+          language
+          forksCount
+          stargazersCount
+          ratingAverage
+          reviewCount
+          ownerAvatarUrl
+        }
+      }
     }
   }
 `
--- part10/rate-repository-app/src/hooks/useRepositories.js
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,24 +1,15 @@
-import { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+
+import { GET_REPOSITORIES } from '../graphql/queries'
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState()
-  const [loading, setLoading] = useState(false)
+  const { loading, data } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  })
 
-  const fetchRepositories = async () => {
-    setLoading(true)
+  const repositories = loading ? null : data.repositories
 
-    const response = await fetch('http://192.168.31.10:5000/api/repositories')
-    const json = await response.json()
-
-    setLoading(false)
-    setRepositories(json)
-  }
-
-  useEffect(() => {
-    fetchRepositories()
-  }, [])
-
-  return { repositories, loading, refetch: fetchRepositories }
+  return { repositories, loading }
 }
 
 export default useRepositories
