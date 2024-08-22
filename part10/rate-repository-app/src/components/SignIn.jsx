3cc9765d2d941ffd3a438aecfaa71fbcaded5702
YANG Shengtao <eranikus424@gmail.com>
2024-08-22 19:56:21 +0800

    Exercise 10.15: storing the access token step2

--- part10/rate-repository-app/src/components/SignIn.jsx
+++ part10/rate-repository-app/src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import { StyleSheet, View } from 'react-native'
+import { useNavigate } from 'react-router-native'
 import { useFormik } from 'formik'
 import * as yup from 'yup'
 
@@ -26,13 +27,14 @@ const validationSchema = yup.object().shape({
 
 const SignIn = () => {
   const [signIn] = useSignIn()
+  const navigate = useNavigate()
 
   const onSubmit = async (values) => {
     const { username, password } = values
 
     try {
-      const { data } = await signIn({ username, password })
-      console.log(data)
+      await signIn({ username, password })
+      navigate('/')
     } catch (e) {
       console.log(e)
     }
--- part10/rate-repository-app/src/hooks/useSignIn.js
+++ part10/rate-repository-app/src/hooks/useSignIn.js
@@ -1,4 +1,4 @@
-import { useMutation } from '@apollo/client'
+import { useApolloClient, useMutation } from '@apollo/client'
 
 import { SIGN_IN } from '../graphql/mutations'
 import useAuthStorage from './useAuthStorage'
@@ -6,9 +6,12 @@ import useAuthStorage from './useAuthStorage'
 const useSignIn = () => {
   const [mutate, result] = useMutation(SIGN_IN)
   const authStorage = useAuthStorage()
+  const apolloClient = useApolloClient()
 
   const signIn = async ({ username, password }) => {
-    const data = await mutate({ variables: { username, password } })
+    const { data } = await mutate({ variables: { username, password } })
+    await authStorage.setAccessToken(data.authenticate.accessToken)
+    apolloClient.resetStore()
     return data
   }
 
