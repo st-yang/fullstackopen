8c781acf9a78faf987f8b6afb78046a57cd53c7b
YANG Shengtao <eranikus424@gmail.com>
2024-08-22 19:52:51 +0800

    part10c Using React Context for dependency injection

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from '@apollo/client'
 import Main from './src/components/Main'
 import createApolloClient from './src/utils/apolloClient'
 import AuthStorage from './src/utils/authStorage'
+import AuthStorageContext from './src/contexts/AuthStorageContext'
 
 const authStorage = new AuthStorage()
 const apolloClient = createApolloClient(authStorage)
@@ -14,7 +15,9 @@ const App = () => {
     <>
       <NativeRouter>
         <ApolloProvider client={apolloClient}>
-          <Main />
+          <AuthStorageContext.Provider value={authStorage}>
+            <Main />
+          </AuthStorageContext.Provider>
         </ApolloProvider>
       </NativeRouter>
       <StatusBar style='auto' />
--- part10/rate-repository-app/src/hooks/useSignIn.js
+++ part10/rate-repository-app/src/hooks/useSignIn.js
@@ -1,8 +1,11 @@
 import { useMutation } from '@apollo/client'
+
 import { SIGN_IN } from '../graphql/mutations'
+import useAuthStorage from './useAuthStorage'
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(SIGN_IN)
+  const authStorage = useAuthStorage()
 
   const signIn = async ({ username, password }) => {
     const data = await mutate({ variables: { username, password } })
