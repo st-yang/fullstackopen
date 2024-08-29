493fb92f64a64f872f453eb99395803a0e8d6be6
YANG Shengtao <eranikus424@gmail.com>
2024-08-29 21:13:35 +0800

    Exercise 13.19.

--- part13/bloglist/models/index.js
+++ part13/bloglist/models/index.js
@@ -1,10 +1,15 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./reading_list')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: ReadingList, as: 'listedBlogs' })
+Blog.belongsToMany(User, { through: ReadingList, as: 'usersListed' })
+
 module.exports = {
   Blog,
   User,
+  ReadingList,
 }
