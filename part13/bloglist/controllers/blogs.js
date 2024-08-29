f3895c579d2a98fa4695c7991a4676ef777990e0
YANG Shengtao <eranikus424@gmail.com>
2024-08-29 14:50:03 +0800

    Exercise 13.6.

--- part13/bloglist/controllers/blogs.js
+++ part13/bloglist/controllers/blogs.js
@@ -36,4 +36,14 @@ router.delete('/:id', blogFinder, async (req, res) => {
   res.status(204).end()
 })
 
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
 module.exports = router
