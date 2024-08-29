125e052f1b95ccf53f3a1a8844161f4becd70525
YANG Shengtao <eranikus424@gmail.com>
2024-08-29 14:37:30 +0800

    part13b Application structuring

---
+++ sql-notes/controllers/notes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router()
+
+const { Note } = require('../models')
+
+router.get('/', async (req, res) => {
+  const notes = await Note.findAll()
+  res.json(notes)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const note = await Note.create(req.body)
+    res.json(note)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    await note.destroy()
+  }
+  res.status(204).end()
+})
+
+router.put('/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    note.important = req.body.important
+    await note.save()
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+module.exports = router
--- sql-notes/index.js
+++ sql-notes/index.js
@@ -1,80 +1,20 @@
-require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
 
-class Note extends Model {}
-Note.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    important: {
-      type: DataTypes.BOOLEAN,
-    },
-    date: {
-      type: DataTypes.DATE,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: 'note',
-  },
-)
-Note.sync()
+const notesRouter = require('./controllers/notes')
 
 app.use(express.json())
 
-app.get('/api/notes', async (req, res) => {
-  const notes = await Note.findAll()
+app.use('/api/notes', notesRouter)
 
-  console.log(notes.map((n) => n.toJSON()))
-  console.log(JSON.stringify(notes, null, 2))
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
 
-  res.json(notes)
-})
-
-app.post('/api/notes', async (req, res) => {
-  try {
-    const note = await Note.create(req.body)
-    return res.json(note)
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
-})
-
-app.get('/api/notes/:id', async (req, res) => {
-  const note = await Note.findByPk(req.params.id)
-  if (note) {
-    console.log(note.toJSON())
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
-})
-
-app.put('/api/notes/:id', async (req, res) => {
-  const note = await Note.findByPk(req.params.id)
-  if (note) {
-    note.important = req.body.important
-    await note.save()
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+start()
