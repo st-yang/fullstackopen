7e009e6af3255da4a2809c0d4850ce777b14dd98
YANG Shengtao <eranikus424@gmail.com>
2024-08-29 19:36:58 +0800

    Exercise 13.18.

--- part13/bloglist/models/blog.js
+++ part13/bloglist/models/blog.js
@@ -26,6 +26,14 @@ Blog.init(
       type: DataTypes.INTEGER,
       defaultValue: 0,
     },
+    year: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: true,
+        min: 1991,
+        max: new Date().getFullYear(),
+      },
+    },
   },
   {
     sequelize,
