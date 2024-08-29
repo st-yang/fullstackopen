efe752023bc1f29d88965d5823ecc341dfc91af3
YANG Shengtao <eranikus424@gmail.com>
2024-08-29 15:12:04 +0800

    part13b User management

--- sql-notes/index.js
+++ sql-notes/index.js
@@ -6,10 +6,14 @@ const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(express.json())
 
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const start = async () => {
   await connectToDatabase()
