631a68e27f6af52f4af1f6e2caf32cc50b961324
YANG Shengtao <eranikus424@gmail.com>
2024-08-30 00:14:20 +0800

    Exercise 13.24. Improve logout

--- part13/bloglist/controllers/logout.js
+++ part13/bloglist/controllers/logout.js
@@ -1,18 +1,12 @@
 const router = require('express').Router()
 
-const { Session, User } = require('../models')
+const { Session } = require('../models')
 const { tokenExtractor } = require('../util/middleware')
 
 router.delete('/', tokenExtractor, async (req, res) => {
-  // directly extract token as already checked in tokenExtractor
-  const token = req.get('authorization').substring(7)
-
-  const session = await Session.findOne({ where: { token } })
-  if (!session) {
-    return res.status(401).json({ error: 'token expired' })
-  }
-
-  await session.destroy()
+  await Session.destroy({
+    where: { userId: req.decodedToken.id },
+  })
   res.status(204).end()
 })
 
