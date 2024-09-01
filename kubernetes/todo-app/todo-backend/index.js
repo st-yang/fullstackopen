a01a2631ae1d31a59aeb74a7ccc9595ae60aadd9
YANG Shengtao <eranikus424@gmail.com>
2024-09-01 21:25:17 +0800

    Exercise 1.05: Project v0.3

--- kubernetes/todo-app/todo-backend/index.js
+++ kubernetes/todo-app/todo-backend/index.js
@@ -3,6 +3,10 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
